f37b9b899f0866fcdba398139dd512ac5913ffe7
Rudi Sherry <rsherry@adobe.com>
2012-12-09 17:14:13 -0800

    Login now preserves the user name if there isn't a match

--- paigow/views.py
+++ paigow/views.py
@@ -8,59 +8,66 @@ from django.template import RequestContext
 from models.pggame import PGGame
 from models.pgplayer import PGPlayer
 
-# utility function to set up the request parameters
-def setup_request_params( request ):
-  # this call makes sure 'messages' appears in whatever page we serve
-  params = RequestContext( request )
-  
-  # this call protects agains cross-site request forgery
+# protect against cross-site request forgery, and put it into a RequestContext
+# so the middleware infrastructure like 'messages' works.
+def request_context( request, params ):
   params.update( csrf( request ) )
-  return params
+  return RequestContext( request, params )
 
 # home page: if they're not logged in, allow them to log in or register.
 # if they are logged in, show them the home page (TBD).
 def home( request ):
-  params = setup_request_params( request )
+  params = {}
   if (not request.session.get('player_id', False)):
     messages.add_message( request, messages.INFO, "You are not logged in." )
-    return render_to_response( 'user_login.html', params )
+    return render_to_response( 'user_login.html', request_context( request, params ) )
   else:
-    return render_to_response( 'base_site.html', params )
+    return render_to_response( 'base_site.html', request_context( request, params ) )
 
 
-# register as a new user
+
+# the user clicked the 'Register' button on the login page
 def register( request ):
-    params = setup_request_params( request )
-    return render_to_response( 'user_login.html', params )
+    params = {}
+    return render_to_response( 'user_login.html', request_context( request, params ) )
 
+# the user clicked the 'Register' button on the login page
 def login( request ):
-  params = setup_request_params( request )
-  player = PGPlayer.objects.filter(name = request.POST['username'])
+  params = {}
+  username = request.POST['username']
+  if ( not username ):
+    messages.add_message(request, messages.ERROR, "Please provide a username.")
+    return render_to_response( 'user_login.html', request_context( request, params ) )
+  
+  player = PGPlayer.objects.filter( name = username )
   if ( not player ):
-    messages.add_message(request, messages.ERROR, "Your username and password didn't match.")
-    return render_to_response( 'user_login.html', params )
+    messages.add_message(request, messages.ERROR, "Your username and/or password didn't match.")
+    params['username'] = username
+    return render_to_response( 'user_login.html', request_context( request, params ) )
   
   if m.password != request.POST['password']:
-    messages.add_message(request, messages.ERROR, "Your username and password didn't match.")
-    return render_to_response( 'user_login.html', params )
+    messages.add_message(request, messages.ERROR, "Your username and/or password didn't match.")
+    return render_to_response( 'user_login.html', request_context( request, params ) )
 
   # remember this player id in the session and send them to the home page.
   request.session['player_id'] = player.id
-  return render_to_response( 'base_site.html', params )
+  return render_to_response( 'base_site.html', request_context( request, params ) )
+
+
 
 # Send a page to create a new game
 def create_game( request ):
-  params = setup_request_params( request )
+  params = {}
   # test if the user is logged in
   if (not request.session.get('player_id', False)):
-    return render_to_response( 'user_login.html', { 'next_page' : 'create_game' } )
+    return render_to_response( 'user_login.html', request_context( request, params ) )
   else:
     new_game = PGGame.create( 'enter name' )
     new_game.save()
-    return render_to_response( 'game_create.html',
-      { 'game': new_game,
-        'title': 'New Game'
-      } )
+    params.extend( { 'game': new_game, 'title': 'New Game' } )
+    return render_to_response( 'game_create.html', request_context( request, params ) )
+
+
 
 # Wrapper view for Templates, may be needed for debugging
 from django.views.generic import TemplateView
